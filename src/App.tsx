9a7f6caf6a2e69f06aa690afb679fe0c993a6b1d
김동일 <ehddlfwkd32@naver.com>
2024-03-22 22:03:22 +0900

    [FIXED] motion mamuri

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,64 @@
 import styled from 'styled-components';
-import React, { useEffect, useRef } from 'react';
-import { motion, useMotionValue, useMotionValueEvent, useScroll, useTransform, useViewportScroll } from 'framer-motion';
+import React, { useEffect, useRef, useState } from 'react';
+import { AnimatePresence, animate, motion } from 'framer-motion';
 
 const Wrapper = styled(motion.div)`
     height: 100vh;
     width: 100vw;
     display: flex;
-    justify-content: center;
+    justify-content: space-around;
     align-items: center;
 `;
-const BiggerBox = styled.div`
-    width: 600px;
-    height: 600px;
-    background-color: rgba(255, 255, 255, 0.4);
-    border-radius: 40px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    overflow: hidden;
+
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    width: 50vw;
+    gap: 10px;
+    div:first-child,
+    div:last-child {
+        grid-column: span 2;
+    }
 `;
 const Box = styled(motion.div)`
-    width: 200px;
     height: 200px;
-    grid-template-columns: repeat(2, 1fr);
     background-color: rgba(255, 255, 255, 1);
     border-radius: 40px;
     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Svg = styled.svg`
-    width: 300px;
-    height: 300px;
-    path {
-        stroke: white;
-        stroke-width: 2;
-    }
+const Overlay = styled(motion.div)`
+    width: 100%;
+    height: 100%;
+
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
 
-const svg = {
-    start: {
-        pathLength: 0,
-        fill: 'rgba(255,255,255,0)',
-    },
-    end: {
-        fill: 'rgba(255,255,255,1)',
-        pathLength: 1,
-    },
-};
 function App() {
+    const [id, setId] = useState<null | string>(null);
+    console.log(id);
     return (
         <Wrapper>
-            <Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                <motion.path
-                    variants={svg}
-                    initial={'start'}
-                    animate={'end'}
-                    transition={{
-                        default: { duration: 5 },
-                        fill: { duration: 1, delay: 2 },
-                    }}
-                    d="M224 373.1c-25.2-31.7-40.1-59.4-45-83.2-22.6-88 112.6-88 90.1 0-5.5 24.3-20.3 52-45 83.2zm138.2 73.2c-42.1 18.3-83.7-10.9-119.3-50.5 103.9-130.1 46.1-200-18.9-200-54.9 0-85.2 46.5-73.3 100.5 6.9 29.2 25.2 62.4 54.4 99.5-32.5 36.1-60.6 52.7-85.2 54.9-50 7.4-89.1-41.1-71.3-91.1 15.1-39.2 111.7-231.2 115.9-241.6 15.8-30.1 25.6-57.4 59.4-57.4 32.3 0 43.4 25.9 60.4 59.9 36 70.6 89.4 177.5 114.8 239.1 13.2 33.1-1.4 71.3-37 86.6zm47-136.1C280.3 35.9 273.1 32 224 32c-45.5 0-64.9 31.7-84.7 72.8C33.2 317.1 22.9 347.2 22 349.8-3.2 419.1 48.7 480 111.6 480c21.7 0 60.6-6.1 112.4-62.4 58.7 63.8 101.3 62.4 112.4 62.4 62.9 .1 114.9-60.9 89.6-130.2 0-3.9-16.8-38.9-16.8-39.6z"
-                />
-            </Svg>
+            <Grid>
+                {['1', '2', '3', '4'].map((e) => (
+                    <Box onClick={() => setId(e)} key={e} layoutId={e} />
+                ))}
+            </Grid>
+            <AnimatePresence>
+                {id ? (
+                    <Overlay
+                        onClick={() => setId(null)}
+                        initial={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}
+                        animate={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
+                        exit={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}
+                    >
+                        <Box layoutId={id} style={{ width: 400, height: 200 }} />
+                    </Overlay>
+                ) : null}
+            </AnimatePresence>
         </Wrapper>
     );
 }
